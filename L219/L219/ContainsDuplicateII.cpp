d32fd874009ec35bafcae69105a2fef1ec40b37b
HooHooE <hoohooe@163.com>
2017-03-11 23:24:50 +0800

    ContainsDuplicateII

---
+++ L219/L219/ContainsDuplicateII.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+	int len = nums.size();
+	if (len == 0) return false;
+	unordered_map<int, int> hashmap;
+	unordered_map<int, int>::const_iterator it;
+	hashmap[nums[0]] = 0;
+	for (int i = 1;i<len;i++) {
+		it = hashmap.find(nums[i]);
+		if (it != hashmap.end()) {
+			if (i - it->second > k) {
+				hashmap[it->first] = i;
+			}
+			else return true;
+		}
+		else {
+			hashmap[nums[i]] = i;
+		}
+	}
+	return false;
+}
+
+int main() {
+	vector<int> nums{ 4,1,2,3,1,5 };
+	bool flag = containsNearbyDuplicate(nums, 3);
+	return 0;
+}
