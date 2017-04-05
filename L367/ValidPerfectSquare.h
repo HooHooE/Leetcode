af6e8910b88950cd21e17d5cfefedc88749b1df9
HooHooE <hoohooe@163.com>
2017-04-05 22:50:09 +0800

    ValidPerfectSquare

---
+++ L367/ValidPerfectSquare.h
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num == 1) return true;
+        long t = num/2;
+		while(t*t > num){
+			t = (t + num/t )/2;
+		}
+		return t*t==num;
+    }
+};
