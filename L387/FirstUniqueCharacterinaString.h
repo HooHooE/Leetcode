277933f119b9e6423b7a848d413119e8bdb64553
HooHooE <hoohooe@163.com>
2017-04-15 23:12:51 +0800

    FirstUniqueCharacterinaString

---
+++ L387/FirstUniqueCharacterinaString.h
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int len = s.size();
+        unordered_map<char,int> hash;
+        for(int i = 0; i<len; i++) hash[s[i]]++;
+        for(int i = 0; i<len; i++) {
+            if(hash[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
